41929623cb999140e731b650e21dbe7ab987c47c
谭杨 <tanyang@xindong.com>
2020-10-19 10:50:48 +0800

    fix(table): fix table default size

--- packages/conf/index.js
+++ packages/conf/index.js
@@ -16,7 +16,7 @@ const GlobalConfig = {
     // showHeaderOverflow: null,
     // showFooterOverflow: null,
     // resizeInterval: 500,
-    size: 'medium',
+    size: 'default',
     // zIndex: null,
     // resizable: false,
     // autoResize: false,
